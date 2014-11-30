94cd864987c534675ed011a31cc3d5a040f13e4e
Lars Greiss <kufikugel@googlemail.com>
2014-11-30 22:19:19 +0100

    Settings: add quick pulldown listpreference
    Change-Id: Ib49ab9906755df8c5011a7272b1a6d566b6e7b6e
    
    Conflicts:
    	src/com/android/settings/desolation/NotificationDrawer.java

--- src/com/android/settings/desolation/NotificationDrawer.java
+++ src/com/android/settings/desolation/NotificationDrawer.java
@@ -16,18 +16,27 @@
 
 package com.android.settings.desolation;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.desolation.qs.QSTiles;
 
-public class NotificationDrawer extends SettingsPreferenceFragment {
+public class NotificationDrawer extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
 
     public static final String TAG = "NotificationDrawer";
 
+    private static final String PREF_QUICK_PULLDOWN = "quick_pulldown";
+
     private Preference mQSTiles;
+    ListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,8 +44,17 @@ public class NotificationDrawer extends SettingsPreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.notification_drawer);
+        PreferenceScreen prefs = getPreferenceScreen();
 
         mQSTiles = findPreference("qs_order");
+
+        mQuickPulldown = (ListPreference) findPreference(PREF_QUICK_PULLDOWN);
+
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
+        mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
+        updateQuickPulldownSummary(statusQuickPulldown);
     }
 
     @Override
@@ -46,5 +64,28 @@ public class NotificationDrawer extends SettingsPreferenceFragment {
         int qsTileCount = QSTiles.determineTileCount(getActivity());
         mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
                     qsTileCount, qsTileCount));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQuickPulldown) {
+            int statusQuickPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else {
+            mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown));
+        }
     }
 }
