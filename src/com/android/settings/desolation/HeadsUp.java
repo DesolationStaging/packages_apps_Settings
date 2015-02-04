84b6a0c660bc0d4b0570f227baed4e57043cd670
Lars Greiss <kufikugel@googlemail.com>
2015-02-04 17:31:12 -0200

    HeadsUp: add timeout option (2/2)
    Conflicts:
    	res/values/deso_arrays.xml
    	src/com/android/settings/desolation/HeadsUp.java
    
    Conflicts:
    	src/com/android/settings/desolation/HeadsUp.java

--- src/com/android/settings/desolation/HeadsUp.java
+++ src/com/android/settings/desolation/HeadsUp.java
@@ -23,14 +23,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.Gravity;
@@ -56,7 +60,8 @@ import java.util.Map;
 
 public class HeadsUp extends SettingsPreferenceFragment
         implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback,
-                AdapterView.OnItemLongClickListener, Preference.OnPreferenceClickListener {
+                AdapterView.OnItemLongClickListener, Preference.OnPreferenceClickListener,
+                            OnPreferenceChangeListener {
 
     private static final int DIALOG_DND_APPS = 0;
     private static final int DIALOG_BLACKLIST_APPS = 1;
@@ -124,6 +129,15 @@ public class HeadsUp extends SettingsPreferenceFragment
                 Settings.System.HEADS_UP_DISMISS_ON_REMOVE, mHeadsUpDismiss ? 1 : 0) == 1);
         mHeadsUpDismissOnRemove.setOnPreferenceChangeListener(this);
         updateHeadsUpDismissOnRemoveSummary(mHeadsUpDismiss);
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_NOTIFCATION_DECAY, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
     }
 
     @Override
@@ -310,6 +324,13 @@ public class HeadsUp extends SettingsPreferenceFragment
                     Settings.System.HEADS_UP_DISMISS_ON_REMOVE, checked ? 1 : 0);
             updateHeadsUpDismissOnRemoveSummary(checked);
             return true;
+        } else if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_NOTIFCATION_DECAY,
+                    headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
         }
         return false;
     }
@@ -471,6 +492,17 @@ public class HeadsUp extends SettingsPreferenceFragment
         } else {
             mHeadsUpDismissOnRemove.setSummary(
                     getResources().getString(R.string.heads_up_dismiss_on_remove_summary_disabled));
+        }
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        if (value == 0) {
+            mHeadsUpTimeOut.setSummary(
+                    getResources().getString(R.string.heads_up_time_out_never_summary));
+        } else {
+            mHeadsUpTimeOut.setSummary(summary);
         }
     }
 }
