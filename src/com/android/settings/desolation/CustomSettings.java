365d76dbc22664ba360c956ceaf4a772a27b8488
REV3NT3CH <rev3nt3ch@gmail.com>
2015-02-24 18:25:52 -0800

    Add Custom Desolation Settings
    Also Add Desolation ROM Logo to about phone section
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/deso_strings.xml
    	src/com/android/settings/Settings.java
    
    Add Desolated Core Icons
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

---
+++ src/com/android/settings/desolation/CustomSettings.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2015 DesolationROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.desolation;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class CustomSettings extends SettingsPreferenceFragment
+        implements OnSharedPreferenceChangeListener {
+
+    private static final String TAG = "CustomSettings";
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.desolation_custom_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        PackageManager pm = getPackageManager();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
