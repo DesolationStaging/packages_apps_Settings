ef971ec0e0f43c3e4377216382c8ba2c1d6e32b6
lirokoa <lirokoa@hotmail.com>
2015-04-04 12:13:28 +0200

    SoundSettings: Fix NPE in sound settings
    Fix a NullPointerException when there is no telephony on the device.
    
    Change-Id: I66994f8f18c2a560b0c370514f61fc45894c9f8b
    
    Conflicts:
    	src/com/android/settings/notification/NotificationSettings.java

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -195,11 +195,13 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
     }
 
     private void updateRingOrNotificationPreference() {
-        mRingOrNotificationPreference.showIcon(mSuppressor != null
-                ? com.android.internal.R.drawable.ic_audio_ring_notif_mute
-                : mRingerMode == AudioManager.RINGER_MODE_VIBRATE
-                ? com.android.internal.R.drawable.ic_audio_ring_notif_vibrate
-                : com.android.internal.R.drawable.ic_audio_ring_notif);
+        if (mRingOrNotificationPreference != null) {
+            mRingOrNotificationPreference.showIcon(mSuppressor != null
+                    ? com.android.internal.R.drawable.ic_audio_ring_notif_mute
+                    : mRingerMode == AudioManager.RINGER_MODE_VIBRATE
+                    ? com.android.internal.R.drawable.ic_audio_ring_notif_vibrate
+                    : com.android.internal.R.drawable.ic_audio_ring_notif);
+         }
     }
 
     private void updateRingerMode() {
