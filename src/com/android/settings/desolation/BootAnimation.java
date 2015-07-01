258c451c8ff6cf8a00223243d9527f5d1e7e04d3
Snuzzo <jrracinfan@gmail.com>
2015-07-01 09:28:16 -0400

    (2/3) BootAnimation; Add Boot Animation Selections
    --Add 8-bit Arcade by @scar45

--- src/com/android/settings/desolation/BootAnimation.java
+++ src/com/android/settings/desolation/BootAnimation.java
@@ -20,37 +20,50 @@ import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemProperties;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+
 import com.android.settings.R;
 import com.android.settings.util.Helpers;
 import com.android.settings.util.CMDProcessor;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class BootAnimation extends SettingsPreferenceFragment {
+public class BootAnimation extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "DesoCore BootAnimations";
 
     public static final String USE_BOOTANIMATION_KEY = "enable_bootanimation";
+    public static final String SET_BOOTANIMATION_KEY = "select_bootanimation";
+    /*-- For future use
+    public String[] mSDCardBootAnims = zipFileFilter("/sdcard/desobootanimations", ".zip");
+    --*/
     
     private SwitchPreference mBootAnimDisable;
-
+    private ListPreference mBootAnimSelect;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         addPreferencesFromResource(R.xml.boot_animation_settings);
-
+        /*-- For future use
+		//System.out.println(Arrays.toString(mSDCardBootAnims)); //Used in personal testing
+		--*/
         mBootAnimDisable = (SwitchPreference) findPreference(USE_BOOTANIMATION_KEY);
+        mBootAnimSelect = (ListPreference) findPreference(SET_BOOTANIMATION_KEY);
+        updateState();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
         updateState();
     }
 
@@ -62,24 +75,65 @@ public class BootAnimation extends SettingsPreferenceFragment {
 
     private void updateState() {
         updateUseBootAnimation();
+        if(mBootAnimDisable.isChecked()){
+			updateBootAnimSelect();
+		} else {
+			removePreference(mBootAnimSelect);
+		}
     }
     
-    void updateSwitchPreference(SwitchPreference switchPreference, boolean value) {
+    private void updateSwitchPreference(SwitchPreference switchPreference, boolean value) {
         switchPreference.setChecked(value);
     }
 
     private void updateUseBootAnimation() {
-        updateSwitchPreference(
-                mBootAnimDisable, SystemProperties.getBoolean("persist.sys.deso.bootanim", true));
+			updateSwitchPreference( mBootAnimDisable, SystemProperties.getBoolean("persist.sys.deso.bootanim", true));
+        if(mBootAnimDisable.isChecked()){
+			updateBootAnimSelect();
+		} else {
+			removePreference(mBootAnimSelect);
+		}
     }
+ 
+    private void updateBootAnimSelect(){
+		String[] entries = {"Stock", "8-bit Arcade by Scar45"};
+		String[] values = {"/vendor/bootanimations/stockbootani.zip", "/vendor/bootanimations/8bitarcade.zip"};
+		mBootAnimSelect.setEntries(entries);
+		mBootAnimSelect.setEntryValues(values);
+		SystemProperties.get("persist.sys.deso.bootanimfile", "/system/media/bootanimation.zip");
+        mBootAnimSelect.setValue(SystemProperties.get("persist.sys.deso.bootanimfile", "/vendor/bootanimations/stockbootani.zip"));
+        mBootAnimSelect.setOnPreferenceChangeListener(this);
+	}   
     
     private void writeUseBootAnimation() {
 		SystemProperties.set( "persist.sys.deso.bootanim",  mBootAnimDisable.isChecked() ?  "1" : "0" );
 		if (Helpers.checkSu() == false){
 			CMDProcessor.canSU();
 		}
-		CMDProcessor.runSuCommand("sh /system/bin/bootanitoggle").getStdout();
+		CMDProcessor.runSuCommand("sh /system/bin/bootani toggle").getStdout();
     }
+    
+    private void writeBootAnimSelect(Object newValue) {
+		int index = mBootAnimSelect.findIndexOfValue((String) newValue);
+		SystemProperties.set("persist.sys.deso.bootanimfile", String.valueOf((String) newValue));
+		mBootAnimSelect.setSummary(mBootAnimSelect.getEntries()[index]);
+		if (Helpers.checkSu() == false){
+			CMDProcessor.canSU();
+		}
+		CMDProcessor.runSuCommand("sh /system/bin/bootani writenew").getStdout();
+	}
+
+    private void removePreference(Preference preference) {
+        getPreferenceScreen().removePreference(preference);
+    }
+    
+    /*-- For future use    
+    private String[] zipFileFilter(String dir, String filter){
+		File files = (new File(dir)).listFiles(filter);
+		String[] ret = files.list();
+		return ret;
+	}
+    --*/
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
@@ -90,4 +144,12 @@ public class BootAnimation extends SettingsPreferenceFragment {
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
+
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		if (preference == mBootAnimSelect) {
+			writeBootAnimSelect(newValue);
+			return true;    
+		}
+		return false;
+	}
 }
