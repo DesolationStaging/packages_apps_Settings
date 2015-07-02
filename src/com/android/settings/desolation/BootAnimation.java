3d08e7bbb914d1144730b981eb57ed6c802f516b
Snuzzo <jrracinfan@gmail.com>
2015-07-02 19:48:05 -0400

    Custom Settings; BootAnimation: Add zip file handling from storage
    PS1 Custom Settings; BootAnimations: Add recursive subdir zip filtering
    
    PS2 Use Ext Download dir if /sdcard/desobootanimations doesn't exist
    
    --Fixes NPE in zipFileFilter method in L#171
    
    Fix file formatting
    
    Can we get a clean-up in aisle 13!

--- src/com/android/settings/desolation/BootAnimation.java
+++ src/com/android/settings/desolation/BootAnimation.java
@@ -18,6 +18,7 @@ package com.android.settings.desolation;
 
 import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.SystemProperties;
 import android.preference.ListPreference;
@@ -26,11 +27,14 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 import com.android.settings.R;
 import com.android.settings.util.Helpers;
@@ -44,21 +48,29 @@ public class BootAnimation extends SettingsPreferenceFragment implements Prefere
     public static final String SET_BOOTANIMATION_KEY = "select_bootanimation";
     private SwitchPreference mBootAnimDisable;
     private ListPreference mBootAnimSelect;
-    
-	private FilenameFilter mZipFilter = new FilenameFilter() {
+    private String mStoragePath;
+    private FilenameFilter mZipFilter = new FilenameFilter() {
 		public boolean accept(File dir, String name) {
 			return name.toLowerCase().endsWith(".zip");
 		}
-	};
+    };
+    private FileFilter mDirFilter = new FileFilter() {
+		public boolean accept(File file) {
+			return file.isDirectory();
+		}
+    };
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+	if (runStorageCheck("/sdcard/desobootanimations") == 1){
+		mStoragePath = "/sdcard/desobootanimations";
+	} else {
+		mStoragePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+	}
         addPreferencesFromResource(R.xml.boot_animation_settings);
         mBootAnimDisable = (SwitchPreference) findPreference(USE_BOOTANIMATION_KEY);
         mBootAnimSelect = (ListPreference) findPreference(SET_BOOTANIMATION_KEY);
-        updateState();
     }
 
     @Override
@@ -75,11 +87,7 @@ public class BootAnimation extends SettingsPreferenceFragment implements Prefere
 
     private void updateState() {
         updateUseBootAnimation();
-        if(mBootAnimDisable.isChecked()){
-			updateBootAnimSelect();
-		} else {
-			removePreference(mBootAnimSelect);
-		}
+        updateBootAnimSelect();
     }
     
     private void updateSwitchPreference(SwitchPreference switchPreference, boolean value) {
@@ -87,63 +95,114 @@ public class BootAnimation extends SettingsPreferenceFragment implements Prefere
     }
 
     private void updateUseBootAnimation() {
-			updateSwitchPreference( mBootAnimDisable, SystemProperties.getBoolean("persist.sys.deso.bootanim", true));
-        if(mBootAnimDisable.isChecked()){
-			updateBootAnimSelect();
-		} else {
-			removePreference(mBootAnimSelect);
-		}
+		updateSwitchPreference( mBootAnimDisable, SystemProperties.getBoolean("persist.sys.deso.bootanim", true));
     }
  
     private void updateBootAnimSelect(){
-		String[] entries = {
-		//If more are added please modify here -- See vendor
-		"Stock", // 1
-		"8-bit Arcade by Scar45", // 2
-		zipFileFilter("/sdcard/desobootanimations")// 3 ---EDIT ABOVE THIS LINE--- Available file(s) from Storage
-		};
-		String[] values = {
-		// & remember to add their matching path -- See vendor
-		"/vendor/bootanimations/stockbootani.zip", // 1
-		"/vendor/bootanimations/8bitarcade.zip", // 2
-		zipFileFilter("/sdcard/desobootanimations")// 3 ---EDIT ABOVE THIS LINE--- Available file(s) from Storage
-		};
-		System.out.println(Arrays.toString(entries));
-		mBootAnimSelect.setEntries(entries);
-		mBootAnimSelect.setEntryValues(values);
-		SystemProperties.get("persist.sys.deso.bootanimfile", "/system/media/bootanimation.zip");
+	List<CharSequence> storagelist = new ArrayList<CharSequence>();
+	List<CharSequence> storagevalues = new ArrayList<CharSequence>();
+	/*--- Available zips from Storage
+	 * --Static path set to /sdcard/desobootanimations */
+	// ---EDIT BELOW THIS LINE FOR STATIC ENTRIES
+	CharSequence[] staticentries = {
+	//If more are added please modify here -- See vendor
+	"Stock", // 1
+	"8-bit Arcade by Scar45" // 2
+	};
+	CharSequence[] staticvalues = {
+	// & remember to add their matching path -- See vendor
+	"/vendor/bootanimations/stockbootani.zip", // 1
+	"/vendor/bootanimations/8bitarcade.zip" // 2
+	};// 3 ---EDIT ABOVE THIS LINE FOR STATIC ENTRIES
+	switch(runStorageCheck(mStoragePath)){
+		case 0:
+			mBootAnimSelect.setEntries(staticentries);
+			mBootAnimSelect.setEntryValues(staticvalues);
+		case 1:
+			for (CharSequence a: staticentries){
+				storagelist.add(a);
+			}
+			for (CharSequence c: staticvalues){
+				storagevalues.add(c);
+			}
+			CharSequence[] storageentries = zipFileFilter(mStoragePath);
+			for (CharSequence b: storageentries){
+				storagelist.add(b);
+			}
+			for (CharSequence d: storageentries){
+				storagevalues.add(d);
+			}
+			CharSequence[] entries = storagelist.toArray(staticentries);
+			CharSequence[] values = storagevalues.toArray(staticvalues);
+			mBootAnimSelect.setEntries(entries);
+			mBootAnimSelect.setEntryValues(values);
+	}
         mBootAnimSelect.setValue(SystemProperties.get("persist.sys.deso.bootanimfile", "/vendor/bootanimations/stockbootani.zip"));
         mBootAnimSelect.setOnPreferenceChangeListener(this);
 	}   
     
     private void writeUseBootAnimation() {
-		SystemProperties.set( "persist.sys.deso.bootanim",  mBootAnimDisable.isChecked() ?  "1" : "0" );
-		if (Helpers.checkSu() == false){
-			CMDProcessor.canSU();
-		}
-		CMDProcessor.runSuCommand("sh /system/bin/bootani toggle").getStdout();
+	SystemProperties.set( "persist.sys.deso.bootanim",  mBootAnimDisable.isChecked() ?  "1" : "0" );
+	if (Helpers.checkSu() == false){
+		CMDProcessor.canSU();
+	}
+	CMDProcessor.runSuCommand("sh /system/bin/bootani toggle").getStdout();
     }
     
     private void writeBootAnimSelect(Object newValue) {
-		int index = mBootAnimSelect.findIndexOfValue((String) newValue);
-		SystemProperties.set("persist.sys.deso.bootanimfile", String.valueOf((String) newValue));
-		mBootAnimSelect.setSummary(mBootAnimSelect.getEntries()[index]);
-		if (Helpers.checkSu() == false){
-			CMDProcessor.canSU();
-		}
-		CMDProcessor.runSuCommand("sh /system/bin/bootani writenew").getStdout();
+	int index = mBootAnimSelect.findIndexOfValue((String) newValue);
+	SystemProperties.set("persist.sys.deso.bootanimfile", String.valueOf((String) newValue));
+	mBootAnimSelect.setSummary(mBootAnimSelect.getEntries()[index]);
+	if (Helpers.checkSu() == false){
+		CMDProcessor.canSU();
 	}
+	CMDProcessor.runSuCommand("sh /system/bin/bootani writenew").getStdout();
+    }
 
     private void removePreference(Preference preference) {
         getPreferenceScreen().removePreference(preference);
     }
 
-    private String zipFileFilter(String path){
-		File f = new File(path);
-		File[] files = f.listFiles(mZipFilter);
-		String ret = Arrays.toString(files).replace("[", "").replace("]", "").trim();
-		return ret;
+    public CharSequence[] zipFileFilter(String path){
+	File f = new File(path);
+	File[] g;
+	g = f.listFiles(mDirFilter);
+	List<CharSequence> ret = new ArrayList<CharSequence>();
+	for (File a: g){
+		File[] h = a.listFiles(mZipFilter);
+		for (File o: h){
+			if (o.isFile()){
+				ret.add(o.getAbsolutePath());
+				Log.i(TAG, " Found file: "+o.getAbsolutePath());
+			}
+		}
 	}
+	for (File b: g){
+		String dir = b.getAbsolutePath();
+		Log.i(TAG, " Found Path: "+dir);
+		zipFileFilter(dir);	
+		}
+		File[] n = f.listFiles(mZipFilter);
+			for (File c: n){
+				if (c.isFile()){
+					ret.add(c.getAbsolutePath());
+					Log.i(TAG, " Found file: "+c.getAbsolutePath());
+				}
+			}
+		CharSequence[] ret2 = new CharSequence[ret.size()];
+		ret2 = ret.toArray(ret2);
+		return ret2;
+	}
+
+    public int runStorageCheck(String path) {
+	File f = new File(path);
+	File[] g = f.listFiles();
+	if (g == null){
+		return 0;
+	} else {
+		return 1;
+	}			
+    }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
@@ -155,11 +214,11 @@ public class BootAnimation extends SettingsPreferenceFragment implements Prefere
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (preference == mBootAnimSelect) {
-			writeBootAnimSelect(newValue);
-			return true;    
-		}
-		return false;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	if (preference == mBootAnimSelect) {
+		writeBootAnimSelect(newValue);
+		return true;    
 	}
+		return false;
+    }
 }
